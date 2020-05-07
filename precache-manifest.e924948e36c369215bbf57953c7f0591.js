c09128a59328250b218bcba23eab4ccb1ea476fc
rxtatx <rachel.thiim@gmail.com>
2020-05-07 15:52:57 -0500

    Updates

---
+++ precache-manifest.e924948e36c369215bbf57953c7f0591.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6c99153d5fe90e715b3342a0fac52f4",
+    "url": "/mothers_day/index.html"
+  },
+  {
+    "revision": "a2b619fab44d6096c4f0",
+    "url": "/mothers_day/static/css/main.dc9d6ba0.chunk.css"
+  },
+  {
+    "revision": "c74d6b0a5e0262b23f8d",
+    "url": "/mothers_day/static/js/2.d475326a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/mothers_day/static/js/2.d475326a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2b619fab44d6096c4f0",
+    "url": "/mothers_day/static/js/main.07f29e16.chunk.js"
+  },
+  {
+    "revision": "737b29dac6006121488b",
+    "url": "/mothers_day/static/js/runtime-main.fdd59fbc.js"
+  }
+]);

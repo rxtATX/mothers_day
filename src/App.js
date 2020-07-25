ca478a31b14d366ff058b206749123d0abdb4bbc
rxtatx <rachel.thiim@gmail.com>
2020-07-25 14:01:00 -0500

    Resolved grid to be more comfortable on iPhone sizes.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import MainPage from './components/MainPage'
 import { GameplayProvider } from './utility/GlobalState'
 import './App.css';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    margin: '3em'
-  },
-}));
-
 function App() {
-  const classes = useStyles();
 
   return (
-    <main className={classes.root}>
+    <main>
       <GameplayProvider>
         <MainPage />
       </GameplayProvider>
